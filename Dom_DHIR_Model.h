3449ca7c21e2f11c4ca3faa56995c27f012418f6
BellaFleury <isabella.s.fleury@gmail.com>
2026-08-13 10:32:21 -0300

    Add files via upload

---
+++ Dom_DHIR_Model.h
@@ -0,0 +1,23 @@
+// Dom_DHIR_Model.h
+// 12/08/2026
+/* Daniel Luís de Amorim Mariano Santos, 
+Henrique Campos Rodrigues, 
+Isabella de Souza Fleury, 
+Rafaella Castro Zandona Alves de Lima */
+
+#ifndef Dom_DHIR_Model_h
+#define Dom_DHIR_Model_h
+
+typedef struct
+{
+    int lado1;
+    int lado2;
+    char status;
+} Peca;
+
+extern Peca pecas[28];
+
+void embaralhar(Peca pecas[], int n);
+void organizar(Peca pecas[]);
+
+#endif
