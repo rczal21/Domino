9e6829add5d9c9920d0f32156239d951482a51fc
BellaFleury <isabella.s.fleury@gmail.com>
2026-08-13 09:36:57 -0300

    Add files via upload

---
+++ Dom_DHIR_PROJETO.cpp
@@ -0,0 +1,17 @@
+// Dom_DHIR_PROJETO - Projeto Domino
+// 12/08/2026
+/*Daniel Luís de Amorim Mariano Santos, 
+Henrique Campos Rodrigues, 
+Isabella de Souza Fleury, 
+Rafaella Castro Zandona Alves de Lima*/
+
+#include "Dom_DHIR_Model.cpp"
+#include "Dom_DHIR_Controller.cpp"
+
+main()
+{
+	Peca();
+	
+}
+
+
