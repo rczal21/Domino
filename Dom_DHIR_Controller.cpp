f926bb3e2c4ed625244cebb0f93bb402077e14c2
BellaFleury <isabella.s.fleury@gmail.com>
2026-08-13 09:38:52 -0300

    Add files via upload

---
+++ Dom_DHIR_Controller.cpp
@@ -0,0 +1,11 @@
+// Dom_DHIR_Controller
+// 12/08/2026
+/* Daniel Luís de Amorim Mariano Santos, 
+Henrique Campos Rodrigues, 
+Isabella de Souza Fleury, 
+Rafaella Castro Zandona Alves de Lima*/
+
+#include "Dom_ DHIR_Controller.h"
+#include "Dom_DHIR_View.cpp"
+
+
