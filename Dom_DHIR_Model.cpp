566d693b6fb58d29ba1439423dc03556593ed6d6
BellaFleury <isabella.s.fleury@gmail.com>
2026-08-13 10:30:02 -0300

    Update Dom_DHIR_Model.cpp

--- Dom_DHIR_Model.cpp
+++ Dom_DHIR_Model.cpp
@@ -6,37 +6,36 @@ Isabella de Souza Fleury,
 Rafaella Castro Zandona Alves de Lima */
 
 #include "Dom_DHIR_Model.h"
+#include <stdlib.h>
+#include <time.h>
 
-#include<time.h>
+// Define de fato o array
+Peca pecas[28];
 
-// Modelo de peca
-struct Peca
-		{
-			int lado1;
-			int lado2;
-			char status;
-		} pecas[28];
-	
 // Embaralha as pecas
 void embaralhar(Peca pecas[], int n){
 	for(int i = 27; i > 0; i--){
-		int j = rand() % (i + 1);	
-			Peca temp = pecas[i];
-			pecas[i] = pecas[j];
-			pecas[j] = temp;
+		int j = rand() % (i + 1);
+		Peca temp = pecas[i];
+		pecas[i] = pecas[j];
+		pecas[j] = temp;
 	}
 }
-	
+
 void organizar(Peca pecas[]){
-		int indice = 0;
-		for(int i = 0; i < 7; i ++){
-			for(int j = i; j < 7; j++){
-				pecas[indice].lado1 = i;
-				pecas[indice].lado2 = j;
-				indice++;
-			}
+	int indice = 0;
+	for(int i = 0; i < 7; i++){
+		for(int j = i; j < 7; j++){
+			pecas[indice].lado1 = i;
+			pecas[indice].lado2 = j;
+			indice++;
 		}
 	}
+}
+
+void inicializarAleatorio() {
+	srand(time(NULL));
+}
 
 void distribuir(Peca pecas[]){
 	for(int i = 0; i < 7; i ++){
