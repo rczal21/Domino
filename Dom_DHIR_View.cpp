7340683b96c45c6cb8819541c70a3880f426b3d9
hCampossr <henriquecamposc2006@gmail.com>
2026-08-14 00:03:47 -0300

    Update Dom_DHIR_View.cpp

--- Dom_DHIR_View.cpp
+++ Dom_DHIR_View.cpp
@@ -13,3 +13,22 @@ void mostrarPecas(Peca pecas[], int n){
         printf(" Peca %d: [%d|%d]\n", i, pecas[i].lado1, pecas[i].lado2);
     }
 }
+
+void mostrarMao(Peca pecas[]){
+		for(int i = 0; i < 28; i ++){
+			if(pecas[i].status == '1') {
+			printf("[%d|%d], ", pecas[i].lado1, pecas[i].lado2);
+			}
+		}
+	}
+
+void menu(Peca pecas[]){
+    int opcao;
+    printf("Modo de jogo (1 ou 2):")
+    scanf("%d", &opcao);
+    if (opcao == 1)
+        jogar(pecas);
+    else
+        printf("Digite um valor válido")
+    
+}
