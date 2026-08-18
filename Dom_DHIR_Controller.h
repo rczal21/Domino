6e62c5ec5891bf73537a04d52f665ba236316a99
Rafaella Lima <rafaellaczal@icloud.com>
2026-08-18 19:25:12 -0300

    Refactor Dom_DHIR_Controller.h with header guards
    Added header guards and defined Peca structure in Dom_DHIR_Controller.h

--- Dom_DHIR_Controller.h
+++ Dom_DHIR_Controller.h
@@ -1,6 +1,22 @@
 // Dom_DHIR_Controller.h
 // 12/08/2026
-/* Daniel Luís de Amorim Mariano Santos, 
+/* Daniel LuÃ­s de Amorim Mariano Santos, 
 Henrique Campos Rodrigues, 
 Isabella de Souza Fleury, 
 Rafaella Castro Zandona Alves de Lima*/
+
+#ifndef Dom_DHIR_Controller_h
+#define Dom_DHIR_Controller_h
+
+typedef struct
+{
+    int lado1;
+    int lado2;
+    char status;
+} Peca;
+
+extern Peca pecas[28];
+
+void jogar(Peca pecas[])
+
+#endif
