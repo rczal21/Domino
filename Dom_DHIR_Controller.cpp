a2a6d77e1d557d5e4488bea3a42ced7d32cb1b5b
BellaFleury <isabella.s.fleury@gmail.com>
2026-08-20 10:21:39 -0300

    Update Dom_DHIR_Controller.cpp

--- Dom_DHIR_Controller.cpp
+++ Dom_DHIR_Controller.cpp
@@ -6,11 +6,10 @@ Isabella de Souza Fleury,
 Rafaella Castro Zandona Alves de Lima*/
 
 #include "Dom_ DHIR_Controller.h"
-#include "Dom_DHIR_View.cpp"
+#include "Dom_DHIR_View.h"
 
 
-	void jogar(Peca pecas[]){
-		for(int i = 0; i < 10; i++)
-		mostrarPecas(pecas, 28);
-		embaralhar(pecas, 28);
-	}
+void jogar(Peca pecas[]){
+	embaralhar(pecas, 28);
+	mostrarPecas(pecas, 28);
+}
