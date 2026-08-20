6987abb731ee65fe548ef597b0c256942756a70b
BellaFleury <isabella.s.fleury@gmail.com>
2026-08-20 09:30:54 -0300

    Update Dom_DHIR_View.h

--- Dom_DHIR_View.h
+++ Dom_DHIR_View.h
@@ -5,21 +5,7 @@ Henrique Campos Rodrigues,
 Isabella de Souza Fleury, 
 Rafaella Castro Zandona Alves de Lima */
 
-#ifndef Dom_DHIR_View_h
-#define Dom_DHIR_View_h
-
-typedef struct
-{
-    int lado1;
-    int lado2;
-    char status;
-} Peca;
-
-extern Peca pecas[28];
-
 void mostrarPecas(Peca pecas[], int n);
 void mostrarMao(Peca pecas[]);
 void menu(Peca pecas[]);
 
-
-#endif
