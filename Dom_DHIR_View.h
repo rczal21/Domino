ae969604785c7144f5bf2d4a8818b6dd5659bb58
Rafaella Lima <rafaellaczal@icloud.com>
2026-08-23 17:53:54 -0300

    add void mostrarRegras

--- Dom_DHIR_View.h
+++ Dom_DHIR_View.h
@@ -1,18 +1,17 @@
-// Dom_DHIR_View.h
-
-/* Daniel Luís de Amorim Mariano Santos, 
-Henrique Campos Rodrigues, 
-Isabella de Souza Fleury, 
+/* Daniel Luís de Amorim Mariano Santos,
+Henrique Campos Rodrigues,
+Isabella de Souza Fleury,
 Rafaella Castro Zandona Alves de Lima */
-
+ 
 #ifndef Dom_DHIR_View_h
 #define Dom_DHIR_View_h
-
+ 
 #include "Dom_DHIR_Model.h"
-
+ 
 void mostrarPecas(Peca pecas[], int n);
 void mostrarMao(Peca pecas[]);
 void menu(Peca pecas[]);
-
+void mostrarRegras();
+ 
 #endif
 
