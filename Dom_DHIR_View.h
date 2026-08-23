5c2eba62350118ce5d345dbc9a917522b8f56670
Rafaella Lima <rafaellaczal@icloud.com>
2026-08-23 16:22:49 -0300

    Add header guards and include statement to Dom_DHIR_View.h

--- Dom_DHIR_View.h
+++ Dom_DHIR_View.h
@@ -1,11 +1,18 @@
 // Dom_DHIR_View.h
-// 12/08/2026
+
 /* Daniel Luís de Amorim Mariano Santos, 
 Henrique Campos Rodrigues, 
 Isabella de Souza Fleury, 
 Rafaella Castro Zandona Alves de Lima */
 
+#ifndef Dom_DHIR_View_h
+#define Dom_DHIR_View_h
+
+#include "Dom_DHIR_Model.h"
+
 void mostrarPecas(Peca pecas[], int n);
 void mostrarMao(Peca pecas[]);
 void menu(Peca pecas[]);
+
+#endif
 
