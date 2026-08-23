654297198faf64c7f0bcf73956fa09d0ff41d4fa
Rafaella Lima <rafaellaczal@icloud.com>
2026-08-23 16:20:00 -0300

    Refactor jogarSolo to jogar and simplify code

--- Dom_DHIR_Controller.cpp
+++ Dom_DHIR_Controller.cpp
@@ -1,23 +1,15 @@
 // Dom_DHIR_Controller
-// 12/08/2026
+
 /* Daniel Luís de Amorim Mariano Santos, 
 Henrique Campos Rodrigues, 
 Isabella de Souza Fleury, 
 Rafaella Castro Zandona Alves de Lima*/
 
 #include "Dom_DHIR_Controller.h"
-#include "Dom_DHIR_Model.h"
-#include "Dom_DHIR_View.cpp"
- 
-void jogarSolo(Peca pecas[]){
-	inicializarMesa();  // REQ09: mesa começa vazia a cada partida
- 
+#include "Dom_DHIR_View.c"
+
+void jogar(Peca pecas[]){
 	for(int i = 0; i < 10; i++)
 		mostrarPecas(pecas, 28);
 	embaralhar(pecas, 28);
-}
- 
-void jogarDoisJogadores(Peca pecas[]){
-// essa função ainda não foi implementada de verdade, só existe
-// pra compilar. O menu() chama ela na opção 2 (modo dois jogadores).
 }
