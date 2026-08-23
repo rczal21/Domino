9ded93442766b33055f8c548c3b8b721c76f5375
Rafaella Lima <rafaellaczal@icloud.com>
2026-08-23 17:24:48 -0300

    Update Dom_DHIR_Controller.c

--- Dom_DHIR_Controller.c
+++ Dom_DHIR_Controller.c
@@ -1,15 +1,30 @@
 // Dom_DHIR_Controller
-
-/* Daniel Luís de Amorim Mariano Santos, 
-Henrique Campos Rodrigues, 
-Isabella de Souza Fleury, 
+// 12/08/2026
+/* Daniel Luís de Amorim Mariano Santos,
+Henrique Campos Rodrigues,
+Isabella de Souza Fleury,
 Rafaella Castro Zandona Alves de Lima*/
-
+ 
 #include "Dom_DHIR_Controller.h"
 #include "Dom_DHIR_View.c"
-
+ 
 void jogar(Peca pecas[]){
-	for(int i = 0; i < 10; i++)
-		mostrarPecas(pecas, 28);
+ 
+	mostrarPecas(pecas, 28);
+ 
 	embaralhar(pecas, 28);
+	mostrarPecas(pecas, 28);
+ 
+	embaralhar(pecas, 28);
+	mostrarPecas(pecas, 28);
+ 
+	embaralhar(pecas, 28);
+	mostrarPecas(pecas, 28);
+ 
+	embaralhar(pecas, 28);
+	mostrarPecas(pecas, 28);
+ 
+	embaralhar(pecas, 28);
+	mostrarPecas(pecas, 28);
 }
+ 
