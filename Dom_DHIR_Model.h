f71b631ff65e819b18a64b850a5da1498c75e6e2
Rafaella Lima <rafaellaczal@icloud.com>
2026-08-24 16:01:31 -0300

    Refactor Dom_DHIR_Model.h with new function and variables

--- Dom_DHIR_Model.h
+++ Dom_DHIR_Model.h
@@ -1,22 +1,33 @@
 // Dom_DHIR_Model.h
 // 12/08/2026
-/* Daniel Luís de Amorim Mariano Santos, 
-Henrique Campos Rodrigues, 
-Isabella de Souza Fleury, 
+/* Daniel Luís de Amorim Mariano Santos,
+Henrique Campos Rodrigues,
+Isabella de Souza Fleury,
 Rafaella Castro Zandona Alves de Lima */
-
+ 
+#ifndef Dom_DHIR_Model_h
+#define Dom_DHIR_Model_h
+ 
 typedef struct
 {
     int lado1;
     int lado2;
     char status;
 } Peca;
-
+ 
 extern Peca pecas[28];
-
+extern int numJogadores;
+extern int mesaEsquerda;
+extern int mesaDireita;
+extern int qtdMesa;
+ 
 void embaralhar(Peca pecas[], int n);
 void gerarPeca(Peca pecas[]);
 void inicializarAleatorio();
 void distribuir(Peca pecas[]);
+void criarMesa();
 
+extern char jogadorComeca;   
+char definirComecar(Peca pecas[]);
+ 
 #endif
