20e3e4b3209f30b8f247982c81a47d95cc6754d1
Rafaella Lima <rafaellaczal@icloud.com>
2026-08-26 10:18:51 -0300

    Rename Dom_DHIR_PROJETO.c to Dom_DHIR_PROJETO.cpp

---
+++ Dom_DHIR_PROJETO.cpp
@@ -0,0 +1,15 @@
+//Dom_DHIR_PROJETO.cpp - Projeto Domino
+
+/*Daniel Luís de Amorim Mariano Santos,
+Henrique Campos Rodrigues,
+Isabella de Souza Fleury,
+Rafaella Castro Zandona Alves de Lima*/
+
+#include "Dom_DHIR_Model.cpp"
+#include "Dom_DHIR-Controller.cpp"
+
+int main {
+	criarPecas();
+	executarJogo();
+
+}
