47b46359d71f2abbb4a4d5ac5281d38120b097af
Rafaella Lima <rafaellaczal@icloud.com>
2026-08-26 15:54:09 -0300

    Add random seed initialization in main function

--- Dom_DHIR_PROJETO.cpp
+++ Dom_DHIR_PROJETO.cpp
@@ -5,10 +5,14 @@ Henrique Campos Rodrigues,
 Isabella de Souza Fleury,
 Rafaella Castro Zandona Alves de Lima*/
 
+#include <stdlib.h>
+#include <time.h>
+
 #include "Dom_DHIR_Model.cpp"
 #include "Dom_DHIR-Controller.cpp"
 
 int main(){
+	srand(time(NULL));
 	gerarPecas();   //Model
 	jogar();        //Controller
 
