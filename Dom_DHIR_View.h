1b3f9fe990ffbfb81552c657e3553d26b0a8802e
hCampossr <henriquecamposc2006@gmail.com>
2026-08-28 15:43:02 -0300

    Update Dom_DHIR_View.h

--- Dom_DHIR_View.h
+++ Dom_DHIR_View.h
@@ -7,4 +7,24 @@ Rafaella Castro Zandona Alves de Lima */
 
 //prototipos das funções do View
 
-void apresentarPecas();
+// Dom_DHIR_View.h
+
+#ifndef DOM_DHIR_VIEW_H
+#define DOM_DHIR_VIEW_H
+
+#include "Dom_DHIR_Model.h"
+
+void mostrarPecas(Peca pecas[], int n);
+void mostrarMao(char jogador);
+int menuPrincipal();
+void mostrarRegras();
+void mostrarQuemComeca(char jogador);
+char subMenu(char jogador);
+void mostrarMesa();
+void mostrarPecaInvalida();
+void mostrarPecaEncaixada(char lado);
+void mostrarPecaNaoEncaixa();
+void mostrarOpcaoInvalida();
+void mostrarVencedor(char jogador);
+
+#endif
