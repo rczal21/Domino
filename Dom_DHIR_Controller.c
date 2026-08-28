dfbf4ab64e0709ae01bfc9dbbcc20e7e86e8999c
hCampossr <henriquecamposc2006@gmail.com>
2026-08-28 15:47:02 -0300

    Update Dom_DHIR_Controller.c

--- Dom_DHIR_Controller.c
+++ Dom_DHIR_Controller.c
@@ -1,17 +1,41 @@
-// Dom_DHIR_Controller
+// Dom_DHIR_Controller.c
 
 /* Daniel Luís de Amorim Mariano Santos,
 Henrique Campos Rodrigues,
 Isabella de Souza Fleury,
-Rafaella Castro Zandona Alves de Lima*/
+Rafaella Castro Zandona Alves de Lima */
 
 #include "Dom_DHIR_Controller.h"
-#include "Dom_DHIR_View.c"
- 
+#include "Dom_DHIR_View.h"
+#include "Dom_DHIR_Model.h"
+
+void prepararJogo(){
+    gerarPeca();
+    embaralhar();
+    distribuir();
+    primeiraJogada();
+}
+
 void jogar(){
-	embaralhar();
-	do {
-		opcao = subMenu();
-	
-	} while (opcao != 0);
+    int opcao;
+    do {
+        opcao = menuPrincipal();
+
+        switch(opcao) {
+            case 1:
+            case 2:
+                prepararJogo();
+                mostrarMesa();
+                mostrarMao('1');
+                break;
+            case 3:
+                mostrarRegras();
+                break;
+            case 0:
+                break;
+            default:
+                mostrarOpcaoInvalida();
+                break;
+        }
+    } while (opcao != 0);
 }
