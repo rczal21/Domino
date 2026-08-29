09c972c3a295bb646ce96e969300d197e1320f71
Rafaella Lima <rafaellaczal@icloud.com>
2026-08-29 01:16:38 -0300

    Implement game execution logic in executarPartida

--- Dom_DHIR_Controller.c
+++ Dom_DHIR_Controller.c
@@ -16,6 +16,82 @@ void prepararJogo(){
     primeiraJogada();
 }
 
+void executarPartida(int modoDeJogo) {
+    prepararJogo();
+    
+    int fimDeJogo = 0;
+    char jogadorAtual = '1';
+    
+    while (fimDeJogo == 0) {
+        mostrarMesa();
+        mostrarMao(jogadorAtual);
+        
+        char acao = subMenu(jogadorAtual);
+        
+        // 1. JOGAR PEÇA
+        if (acao == 'J' || acao == 'j') {
+            int pecaEscolhida = lerIndicePeca(); 
+            char ladoEscolhido = lerLadoPeca();  
+            
+            if (pecaEscolhida < 0 || pecaEscolhida > 27 || pecas[pecaEscolhida].status != jogadorAtual) {
+                mostrarPecaInvalidaMao(); // <--- Chamada para a View
+                continue; 
+            }
+
+            int jogadaValida = tentarJogar(pecaEscolhida, ladoEscolhido);
+            
+            if (jogadaValida == 1) {
+                mostrarPecaEncaixada(ladoEscolhido);
+                
+                if (contarPecas(jogadorAtual) == 0) {
+                    mostrarVencedor(jogadorAtual);
+                    fimDeJogo = 1; 
+                    continue;
+                }
+            } else {
+                mostrarPecaNaoEncaixa();
+                continue; 
+            }
+        } 
+        
+        // 2. COMPRAR PEÇA
+        else if (acao == 'C' || acao == 'c') {
+            int sucesso = comprarPeca(jogadorAtual); 
+            
+            if (sucesso == 1) {
+                mostrarCompraSucesso(); // <--- Chamada para a View
+            } else {
+                mostrarMonteVazio(); // <--- Chamada para a View
+            }
+            continue; 
+        } 
+        
+        // 3. PASSAR A VEZ
+        else if (acao == 'P' || acao == 'p') {
+            mostrarPassouVez(jogadorAtual); // <--- Chamada para a View
+        }
+        
+        // 4. SAIR DO JOGO
+        else if (acao == 'S' || acao == 's') {
+            mostrarSaindoDoJogo(); // <--- Chamada para a View
+            fimDeJogo = 1; 
+            continue;
+        }
+        
+        else {
+            mostrarOpcaoInvalida();
+            continue; 
+        }
+        
+        // TROCA O TURNO 
+        if (jogadorAtual == '1') {
+            jogadorAtual = '2';
+        } else {
+            jogadorAtual = '1';
+        }
+    }
+}
+
 void jogar(){
     int opcao;
     do {
@@ -24,10 +100,8 @@ void jogar(){
         switch(opcao) {
             case 1:
             case 2:
-                prepararJogo();
-                mostrarMesa();
-                mostrarMao('1');
-                break;
+               executarPartida(opcao); 
+                break; 
             case 3:
                 mostrarRegras();
                 break;
