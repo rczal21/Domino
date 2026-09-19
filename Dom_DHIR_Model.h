86c0bb6b1abd688535fe1140e835aed80ab148a9
Rafaella Lima <rafaellaczal@icloud.com>
2026-09-19 19:35:01 -0300

    Refactor Dom_DHIR_Model.h with comments and includes
    Updated comments and added includes for boolean support.

--- Dom_DHIR_Model.h
+++ Dom_DHIR_Model.h
@@ -1,6 +1,6 @@
 // Dom_DHIR_Model.h
-
-/* Daniel Luís de Amorim Mariano Santos,
+//22/09/2026
+/* Daniel Luis de Amorim Mariano Santos,
 Henrique Campos Rodrigues,
 Isabella de Souza Fleury,
 Rafaella Castro Zandona Alves de Lima */
@@ -8,13 +8,27 @@ Rafaella Castro Zandona Alves de Lima */
 #ifndef DOM_DHIR_MODEL_H
 #define DOM_DHIR_MODEL_H
 
+#include <stdbool.h>
+
 typedef struct {
     int ladoE;
     int ladoD;
     char status;
 } Peca;
 
+//estrutura que guarda a "situacao geral" do jogo no momento de salvar/carregar
+typedef struct {
+    int qtmesaJogo;   // qtde de pecas na mesa (qtdeMesa)
+    char jogadorJogo; // de quem eh a vez ('1' ou '2')
+    int modoJogo;     // 1 = contra o computador, 2 = dois jogadores
+    int mesaDJogo;    // extremidade direita da mesa (mesaD)
+    int mesaEJogo;    // extremidade esquerda da mesa (mesaE)
+    int pontaEJogo;   // ponta esquerda do vetor mesaPecas (pontaE)
+    int pontaDJogo;   // ponta direita do vetor mesaPecas (pontaD)
+} SituacaoJogo;
+
 extern Peca pecas[28];
+extern SituacaoJogo sitJogo;
 extern int qtdeJogadores;
 extern int mesaE;
 extern int mesaD;
