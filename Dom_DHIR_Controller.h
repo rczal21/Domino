27d1291c8321a244fe3edabed857fed603621915
Rafaella Lima <rafaellaczal@icloud.com>
2026-09-19 19:29:01 -0300

    Add new functions for game control in header file

--- Dom_DHIR_Controller.h
+++ Dom_DHIR_Controller.h
@@ -1,12 +1,9 @@
 // Dom_DHIR_Controller.h
-
-/* Daniel Luís de Amorim Mariano Santos, 
+//22/09/2026
+/* Daniel Luis de Amorim Mariano Santos, 
 Henrique Campos Rodrigues, 
 Isabella de Souza Fleury, 
 Rafaella Castro Zandona Alves de Lima*/
-
-
-//Biblioteca das funcoes da main e controller
 
 #ifndef DOM_DHIR_CONTROLLER_H
 #define DOM_DHIR_CONTROLLER_H
@@ -16,6 +13,10 @@ Rafaella Castro Zandona Alves de Lima*/
 void jogar();
 void prepararJogo();
 void executarPartida(int modoDeJogo);
+void rodarPartida(int modoDeJogo, char jogadorInicial);
+void continuarPartida();
+int gravarJogo(char jogadorAtual, int modoDeJogo);
+int carregarJogo();
 
 
 #endif
